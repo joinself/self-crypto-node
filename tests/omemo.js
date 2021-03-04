41e185ca2596ac3fcaa196bdf58eff68080a29bc
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2021-03-04 14:09:28 +0100

    aliceAccount to create_inbound_session

--- tests/omemo.js
+++ tests/omemo.js
@@ -9,30 +9,41 @@ test('group session encrypt decrypt test', function (t) {
   var aliceAccount = crypto.create_olm_account()
   var bobAccount = crypto.create_olm_account()
 
-  crypto.create_account_one_time_keys(bobAccount, 100)
+  // crypto.create_account_one_time_keys(bobAccount, 100)
+  // var bobsIdentityKey = JSON.parse(crypto.identity_keys(bobAccount))['curve25519']
+  // var bobsOneTimeKey = JSON.parse(crypto.one_time_keys(bobAccount))['curve25519']['AAAAAQ']
 
-  var bobsIdentityKey = JSON.parse(crypto.identity_keys(bobAccount))['curve25519']
-  var bobsOneTimeKey = JSON.parse(crypto.one_time_keys(bobAccount))['curve25519']['AAAAAQ']
+  // Force reeal values for all inputs
+  var bobsIdentityKey = 'FPKOZgWjnnqA6IaG7d2odNBV0TdltnQhDbXPTq7xSMM'
+  var bobsOneTimeKey = 'SxhwElBIqJmd1PG/YaCApGiXp5zzSGqm7IoYdkNW6kk'
+  var bobID = '10377379035:dA4mRcImQ4KPJNsqPR3oXe'
+  var aliceID = 'abb6f604-b28d-4c15-ac01-84df3305cf92:1'
 
+  // ENCRYPTION (alice)
   var sessionWithBob = crypto.create_outbound_session(aliceAccount, bobsIdentityKey, bobsOneTimeKey)
   t.ok(sessionWithBob)
 
-  var groupSessionWithBob = crypto.create_group_session('alice')
-  crypto.add_group_participant(groupSessionWithBob, 'bob', sessionWithBob)
+  var groupSessionWithBob = crypto.create_group_session(aliceID)
+  crypto.add_group_participant(groupSessionWithBob, bobID, sessionWithBob)
 
+  console.log(" - encrypting 'hello bob and everyone else'")
   var groupCiphertext = crypto.group_encrypt(groupSessionWithBob, 'hello bob and everyone else')
   t.ok(groupCiphertext)
+  console.log(` - encrypted '${groupCiphertext}'`)
 
-  var ciphertextForBob = JSON.parse(groupCiphertext)['recipients']['bob']['ciphertext']
+  // DECRYPTING (bob)
+  var ciphertextForBob = JSON.parse(groupCiphertext)['recipients'][bobID]['ciphertext']
   t.ok(ciphertextForBob)
 
   var sessionWithAlice = crypto.create_inbound_session(bobAccount, ciphertextForBob)
   t.ok(sessionWithAlice)
 
-  var groupSessionWithAlice = crypto.create_group_session('bob')
-  crypto.add_group_participant(groupSessionWithAlice, 'alice', sessionWithAlice)
+  var groupSessionWithAlice = crypto.create_group_session(bobID)
+  crypto.add_group_participant(groupSessionWithAlice, aliceID, sessionWithAlice)
 
-  var plaintextForBob = crypto.group_decrypt(groupSessionWithAlice, 'alice', groupCiphertext)
+  console.log(` - decrypting '${groupCiphertext}'`)
+  var plaintextForBob = crypto.group_decrypt(groupSessionWithAlice, aliceID, groupCiphertext)
+  console.log(` - decrypted '${plaintextForBob}'`)
 
   t.equal(plaintextForBob, 'hello bob and everyone else')
 
